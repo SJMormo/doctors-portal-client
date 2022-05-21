7a1c15c3dc336935f729d4a43e6e7b345d14124b
SJMormo <sjm91097@gmail.com>
2022-05-21 17:52:30 +0600

    lifed up state to share data

--- src/Pages/Appointment/AppointmentBanner.js
+++ src/Pages/Appointment/AppointmentBanner.js
@@ -1,20 +1,20 @@
-import React, { useState } from 'react';
 import chair from '../../assets/images/chair.png';
 import { DayPicker } from 'react-day-picker';
 import 'react-day-picker/dist/style.css';
 
-const AppointmentBanner = () => {
-    const [date, setDate] = useState(new Date());
+const AppointmentBanner = ({ date, setDate }) => {
+
     return (
         <div class="hero min-h-screen">
             <div class="hero-content flex-col lg:flex-row-reverse">
                 <img src={chair} alt="Dentist Chair" />
                 <div>
-                    <DayPicker 
+                    <DayPicker
                         mode='single'
                         selected={date}
                         onSelect={setDate}
                     />
+                    
                 </div>
             </div>
         </div>
