1d07d3d82ddbdf060c1a7b3b1fd2458ce7beddff
SJMormo <sjm91097@gmail.com>
2022-05-21 20:37:59 +0600

    available services section added

--- src/Pages/Appointment/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments.js
@@ -1,10 +1,27 @@
 import { format } from 'date-fns';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Service from './Service';
 
 const AvailableAppointments = ({ date }) => {
+    const [services, setServices] = useState([]);
+
+    useEffect(() => {
+        fetch('services.json')
+            .then(res => res.json())
+            .then(data => setServices(data));
+    }, []);
+
     return (
         <div>
             <p className='text-xl text-secondary text-center'>Available appointments on: {format(date, 'PP')}</p>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+                {
+                    services.map(service => <Service
+                        key={service._id}
+                        service={service}
+                    />)
+                }
+            </div>
         </div>
     );
 };
