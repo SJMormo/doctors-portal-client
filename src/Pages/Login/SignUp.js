b0ba35b04f3898e4680235683fc7d740ba483351
SJMormo <sjm91097@gmail.com>
2022-05-26 10:01:02 +0600

    user collection added

--- src/Pages/Login/SignUp.js
+++ src/Pages/Login/SignUp.js
@@ -4,6 +4,7 @@ import auth from '../../firebase.init';
 import { useForm } from "react-hook-form";
 import Loading from '../Shared/Loading';
 import { Link, useNavigate } from 'react-router-dom';
+import useToken from '../../Hooks/useToken';
 
 const SignUp = () => {
     const [
@@ -15,15 +16,16 @@ const SignUp = () => {
     const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
     const [updateProfile, updating, updateError] = useUpdateProfile(auth);
 
+    const [token] = useToken(user || gUser)
+
     const navigate = useNavigate();
 
     const { register, formState: { errors }, handleSubmit } = useForm();
 
     let signInError;
 
-    if (user || gUser) {
-        console.log(user || gUser);
-        // navigate('/appointment');
+    if (token) {
+        navigate('/appointment');
     }
 
     if (loading || gLoading || updating) {
@@ -38,7 +40,7 @@ const SignUp = () => {
         await createUserWithEmailAndPassword(data.email, data.password);
         await updateProfile({ displayName: data.name });
         console.log("Update Done");
-        
+
     };
     return (
         <div className='flex h-screen justify-center items-center'>
